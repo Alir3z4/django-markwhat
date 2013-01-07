02063c592479b579ec7dc054a2322fbd09bbaaec
Alireza Savand <alireza.savand@gmail.com>
2013-01-08 00:48:52 +0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name='django-markwhat',
+    version=".".join(map(str, __import__('django_markwhat').__version__)),
+    packages=['django_markwhat', 'django_markwhat.templatetags'],
+    url='http://pypi.python.org/pypi/django-markwhat',
+    license=open('LICENSE').read(),
+    author='Alireza Savand',
+    author_email='alireza.savand@gmail.com',
+    description='A collection of template filters that implement common markup languages.',
+    long_description=open('README.rst').read(),
+    keywords=[
+        'django',
+        'markdown',
+        'markup',
+        'textile',
+        'rst',
+        'reStructuredText',
+        'docutils',
+        'web'
+    ],
+    platforms='OS Independent',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development'
+    ],
+)
