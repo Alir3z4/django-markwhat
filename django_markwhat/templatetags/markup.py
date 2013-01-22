c19630931705df1e18d077cce30e31f148ec70cd
Alireza Savand <alireza.savand@gmail.com>
2013-01-22 22:47:10 +0400

    Updated ChangeLog.rst

--- django_markwhat/templatetags/markup.py
+++ django_markwhat/templatetags/markup.py
@@ -72,7 +72,7 @@ def restructuredtext(value):
     =======================
     """
     from docutils.core import publish_parts
-    
+
     docutils_settings = getattr(settings, "RESTRUCTUREDTEXT_FILTER_SETTINGS", {})
     parts = publish_parts(source=smart_str(value), writer_name="html4css1", settings_overrides=docutils_settings)
     return mark_safe(force_unicode(parts["fragment"]))
