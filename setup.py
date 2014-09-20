741733ff0c7041520e7080e8782a73517b358db2
Alireza Savand <alireza.savand@gmail.com>
2014-09-20 15:57:36 +0400

    PEP8 followed ;)

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setup(
     license=open('LICENSE').read(),
     author='Alireza Savand',
     author_email='alireza.savand@gmail.com',
-    install_requires=['Django',],
-    description='A collection of template filters that implement common markup languages.',
+    install_requires=['Django', ],
+    description="""A collection of template filters that implement
+     common markup languages.""",
     long_description=open('README.rst').read(),
     keywords=[
         'django',
